ca0582b666eb75a6c890c3fb99ad1addc2256e14
jianhua1 <549079173@qq.com>
2019-07-02 18:12:58 +0800

    111

--- src/main/java/com/jh/myspringboot1/service/TOrderService.java
+++ src/main/java/com/jh/myspringboot1/service/TOrderService.java
@@ -3,8 +3,13 @@ package com.jh.myspringboot1.service;
 import com.jh.myspringboot1.dao.TOrderDao;
 import com.jh.myspringboot1.modal.TOrder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.interceptor.TransactionAspectSupport;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.List;
 
 @Service
@@ -12,8 +17,25 @@ public class TOrderService {
     @Autowired
     private TOrderDao tOrderDao;
 
+
     public List<TOrder> getAll(){
         return tOrderDao.getAll();
+    }
+
+    public List<TOrder> getByCondition(TOrder tOrder){
+        return tOrderDao.getByCondition(tOrder);
+    }
+
+
+    @Transactional(rollbackFor = Exception.class)
+    public void addTest2(TOrder tOrder) throws Exception{
+            System.out.println("+++++aaaaaaaaa");
+            tOrderDao.save(tOrder);
+         /*   File f=new File("k:/xxx");
+            f.createNewFile();
+            FileOutputStream fileOutputStream=new FileOutputStream(f);
+            fileOutputStream.write(new byte[1024]);
+            System.out.println("jjjjjjjjjjj");*/
     }
 
 }
