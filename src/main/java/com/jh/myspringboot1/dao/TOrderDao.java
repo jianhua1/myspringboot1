405ae992fd0d1c300d6a21986660646a806c4c6e
jianhua1 <549079173@qq.com>
2019-07-02 18:12:25 +0800

    111

--- src/main/java/com/jh/myspringboot1/dao/TOrderDao.java
+++ src/main/java/com/jh/myspringboot1/dao/TOrderDao.java
@@ -2,11 +2,19 @@ package com.jh.myspringboot1.dao;
 
 import com.jh.myspringboot1.modal.TOrder;
 import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Mapper
+@Repository
 public interface TOrderDao {
+
+
+     int save(TOrder tOrder);
+
      List<TOrder> getAll();
+     List<TOrder> getByCondition(TOrder tOrder);
+
+
 }
